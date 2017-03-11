7f1ccff0f4882c8b3228cc6d4b315bb4ff713631
Procrastinator <aayanl@users.noreply.github.com>
2017-03-10 21:12:42 -0500

    Add callbacks
    I fuckin love callbacks man

--- public/js/main.js
+++ public/js/main.js
@@ -1,22 +1,27 @@
 window.onload = function () {
-  requestCMC();
+  requestCMC(function (data, err){
+    if (err) console.log (err); else {
+      console.log("Got zclassic price")
+      document.getElementById('price').innerHTML = "BTC: "+ data[0].price_btc + "; USD: " + data[0].price_usd;
+    }
+  });
 }
 
-function requestCMC () {
+function requestCMC (cback) {
   var request = new XMLHttpRequest();
   request.open('GET', 'https://api.coinmarketcap.com/v1/ticker/zclassic/', true);
 
   request.onload = function() {
     if (request.status >= 200 && request.status < 400) {
       var data = JSON.parse(request.responseText);
-      document.getElementById('price').innerHTML = "BTC: "+ data[0].price_btc + "; USD: " + data[0].price_usd;
+      cback(data, null)
     } else {
-
+      cback(null, "Connected to CoinMarketCap but error getting zclassic price")
     }
   };
 
   request.onerror = function() {
-    console.log("error getting zclassic price");
+    cback(null, "Error getting zclassic price, possibly couldn't connect to CoinMarketCap")
   };
 
   request.send();
